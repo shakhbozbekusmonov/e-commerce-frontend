bb34ac87fc0f8ef4b2aa62f20ee2144574479f34
shakhbozbekusmonov <miracle_0319@bk.ru>
2024-04-26 18:54:48 +0500

    bug: fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
 import Register from './components/Register'
-=======
-import { Route, Routes } from 'react-router-dom'
-import Login from './components/Login'
->>>>>>> 4dfff8be5ad1064d3704f08fc89337f4ad0f87c9
 
 const App = () => {
 	return (
 		<>
-<<<<<<< HEAD
 			<Register />
-=======
-			<Login />
->>>>>>> 4dfff8be5ad1064d3704f08fc89337f4ad0f87c9
 			{/* <Routes>
 				<Route path='/' element={<h1>Home</h1>} />
 				<Route path='*' element={<h1>Not Found</h1>} />
